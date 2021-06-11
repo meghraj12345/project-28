66a7f24e8f9562092f576dc1929ddb31fae8499c
meghraj12345 <81573070+meghraj12345@users.noreply.github.com>
2021-06-11 14:55:23 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,49 @@
+class launcher{
+    constructor(body,anchor){
+        var options = {
+            bodyA: body,
+            pointB: anchor,
+            stiffness: 0.04,
+            length: 1
+        }
+        this.bodyA = body;
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+    
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+        image(this.launcher1,200,20);
+        image(this.launcher2,170,20);
+        if(this.launcher.bodyA){
+            var pointA = this.launcher.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.sling3,pointA.x -30, pointA.y -10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.launcher3,pointA.x + 25, pointA.y -10,15,30);
+            }
+           
+            
+            pop();
+        }
+    }
+    
+}
